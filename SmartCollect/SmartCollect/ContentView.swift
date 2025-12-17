b8f8b5ca3e90142ac53e40a73a10b02bab476db1
Sachethds <sachethds@gmail.com>
2025-12-17 10:23:49 +0530

    Add files via upload

---
+++ SmartCollect/SmartCollect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartCollect
+//
+//  Created by Sacheth D S on 17/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
